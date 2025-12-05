596e5ccfcc39d4f804273d92c8e4c36f5d1d19a4
pebble_na_bato <feevolinto123@gmail.com>
2025-12-06 02:43:16 +0800

    drafted the part 1, just needs to be further refined and add the analyzation of patterns of cacao usage

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
+//Fuction Prototypes
+int OneWeekCacao();
+
+//Initialization
+float WeekUsage[7];
+float total = 0;
+float average;
+float *ptrWU = WeekUsage; //Pointer to Array of Weekly Usage
+
 int main () {
 
-    int jed = 10;
-    int feevol = 20;
+    OneWeekCacao();
 
-    printf("Jed plus feevol is %d", jed+feevol);
-    //yes
+    return 0;
+}
+
+//Part 1
+//For Further Refinement
+int OneWeekCacao (){
+    puts ("Welcome to philippine airlines.");
+    for (int i=0; i<7; i++){
+        printf("Enter your usage for day %d in kg", i+1);
+        scanf("%f", (ptrWU + i));
+        total += *(ptrWU + i); 
+    }
+
+    average = total / 7;
+    printf("Average usage is %2f", average);
+
+
 
     return 0;
 }
